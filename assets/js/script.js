cc668b0da0047ad22765f72d296dfc30c9a2a197
Prabhashearee <prabhashwaree57@gmail.com>
2022-04-13 21:52:30 +0530

    Update js commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,23 @@ var jelly=["Blue", "Orange", "Green", "Yellow", "Red", "Purple"];
 var board=[];
 var col=9;
 var row=9;
+
+window.onload=function(){
+
+}
+
+function startGame(){
+    for (let index = 0; index < row; index++) {
+       let row=[];
+       for (let i = 0; i < col; i++) {
+           let tittle=document.createElement("img");
+           tittle.id=index.toString()+"-"+i.toString();
+           tittle.src="./images/"+".png"
+           
+
+        //    drag function
+        
+       }
+        
+    }
+}
